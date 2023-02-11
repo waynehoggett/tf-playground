55443ef9b6fb84083f9cf86604243fef37d1663b
Wayne Hoggett <whoggett@auscript.com.au>
2023-02-12 05:32:37 +1000

    Create Resource Group

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tf-playground
+# Terraform Playground
---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  cloud {
+    organization = "WayneHoggett"
+    workspaces {
+      name = "tf-playground"
+    }
+  }
+  required_providers {
+    azurerm = {
+      "source" = "hashiscorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "linuxtestvm" {
+  name = "rg-linuxtestvm-dev-001"
+  location = "Australia East"
+}
