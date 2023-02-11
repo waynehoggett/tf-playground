becc7556d766f14dc031bff73f7c094327e1642c
Wayne Hoggett <whoggett@auscript.com.au>
2023-02-12 06:24:12 +1000

    nsg and rule

--- main.tf
+++ main.tf
@@ -32,5 +32,29 @@ resource "azurerm_virtual_network" "linuxtestvm" {
     name           = "snet-servers"
     address_prefix = "10.0.0.0/24"
   }
+
   tags = var.tags
+
+}
+
+resource "azurerm_network_security_group" "ssh" {
+  name                = "nsg-sshallow-001"
+  resource_group_name = azurerm_resource_group.linuxtestvm.name
+  location            = azurerm_resource_group.linuxtestvm.location
+
+  security_rule {
+    name                       = "allow-inbound-ssh"
+    priority                   = 100
+    description                = "Allow inbound SSH"
+    access                     = "Allow"
+    direction                  = "Inbound"
+    destination_address_prefix = "VirtualNetwork"
+    destination_port_range     = "22"
+    protocol                   = "Tcp"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+  }
+
+  tags = var.tags
+
 }
